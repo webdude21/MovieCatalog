3f066fb8254c0497d0ed20b65579d67a6866063e
webdude <dimo.petrof@gmail.com>
2017-03-18 12:28:50 +0200

    cover pagination with e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor/built';
 import { MovieCatalogPage } from './app.po';
 
 describe('movie-catalog App', () => {
@@ -12,8 +13,17 @@ describe('movie-catalog App', () => {
     expect(page.getParagraphText()).toEqual('Movie Catalog');
   });
 
-  it('using the search should yield results', () => {
+  it('using the search should yield a page with 10 results', () => {
     page.doSearch('Die hard');
+    expect(page.getMoviesResultCount()).toEqual(10);
     expect(page.getResultMovieTitle(1)).toEqual('Die Hard');
+  });
+
+  it('clicking on a page number in the paginator navigates to another page in the search results', () => {
+    page.doSearch('Die hard');
+    page.goToPage(2);
+    browser.waitForAngular();
+    expect(page.getMoviesResultCount()).toEqual(10);
+    expect(page.getResultMovieTitle(1)).toEqual('Angels Die Hard');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -2,6 +2,9 @@ import { protractor } from 'protractor/built/ptor';
 import { browser, element, by } from 'protractor';
 
 export class MovieCatalogPage {
+  private static movieList = 'app-root app-movie-search app-movie-list';
+  private static movieListItems = `${MovieCatalogPage.movieList} ul li`;
+
   navigateTo() {
     return browser.get('/');
   }
@@ -16,6 +19,22 @@ export class MovieCatalogPage {
   }
 
   getResultMovieTitle(index: number) {
-    return element(by.css(`app-root app-movie-search app-movie-list ul li:nth-child(${index}) app-movie-list-item a`)).getText();
+    return element
+      .all(by.css(MovieCatalogPage.movieListItems))
+      .get(index - 1)
+      .all(by.css('app-movie-list-item a'))
+      .get(0)
+      .getText();
+  }
+
+  getMoviesResultCount() {
+    return element.all(by.css(MovieCatalogPage.movieListItems)).count();
+  }
+
+  goToPage(index: number): void {
+    element
+      .all(by.css(`${MovieCatalogPage.movieList} p-datalist p-paginator .ui-paginator-pages a`))
+      .get(index - 1)
+      .click();
   }
 }
