9e75b0a5039dfbba3bd8d8e8203a0a1682c4c76f
webdude <dimo.petrof@gmail.com>
2017-03-19 22:01:19 +0200

    fixed a couple more tests

--- src/app/movie-search/movie-list-item/movie-list-item.component.spec.ts
+++ src/app/movie-search/movie-list-item/movie-list-item.component.spec.ts
@@ -1,3 +1,8 @@
+import { DataListModule } from 'primeng/primeng';
+import { RouterTestingModule } from '@angular/router/testing';
+import { APP_BASE_HREF } from '@angular/common';
+import { Observable } from 'rxjs/Rx';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieListItemComponent } from './movie-list-item.component';
@@ -7,17 +12,24 @@ describe('MovieListItemComponent', () => {
   let fixture: ComponentFixture<MovieListItemComponent>;
   let nativeElement: any;
   const testMovie = {
-    'title': 'Romeo Must Die',
-    'year': '2000',
-    'imdbID': 'tt0165929',
-    'imdbLink': 'testLink',
-    'type': 'movie',
-    'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI5Nzg1MjA5M15BMl5BanBnXkFtZTYwNzAxNzg2._V1_SX300.jpg'
+    title: 'Romeo Must Die',
+    year: '2000',
+    imdbID: 'tt0165929',
+    imdbLink: 'testLink',
+    type: 'movie',
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI5Nzg1MjA5M15BMl5BanBnXkFtZTYwNzAxNzg2._V1_SX300.jpg'
   };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MovieListItemComponent]
+      imports: [RouterTestingModule.withRoutes([]), RouterModule, DataListModule],
+      declarations: [MovieListItemComponent],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, {
+        provide: ActivatedRoute,
+        useValue: {
+          params: Observable.of({ id: testMovie.imdbID })
+        }
+      }]
     }).compileComponents();
   }));
 
@@ -40,7 +52,7 @@ describe('MovieListItemComponent', () => {
   it('should render title and imbd link in an anchor tag', async(() => {
     const anchorTag = nativeElement.querySelector('a');
     expect(anchorTag.textContent).toContain(testMovie.title);
-    expect(anchorTag.href).toContain(testMovie.imdbLink);
+    expect(anchorTag.href).toContain(testMovie.imdbID);
   }));
 
   it('should render poster in an img tag with an alt attribute the title of the movie', async(() => {
--- src/app/movie-search/movie-search/movie-search.component.spec.ts
+++ src/app/movie-search/movie-search/movie-search.component.spec.ts
@@ -1,10 +1,14 @@
-import { APP_BASE_HREF } from '@angular/common';
+import { InputTextModule } from 'primeng';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { Observable } from 'rxjs/Rx';
-import { Movie } from '../model/movie';
-import { HttpModule } from '@angular/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import * as TypeMoq from 'typemoq';
+import { DataListModule } from 'primeng/primeng';
 
+import { PageableEntity } from '../model/pagable-entity';
+import { Movie } from '../model/movie';
 import { MovieService } from '../service/movie.service';
 import { MovieListItemComponent } from '../movie-list-item/movie-list-item.component';
 import { MovieListComponent } from '../movie-list/movie-list.component';
@@ -14,18 +18,25 @@ describe('MovieSearchComponent', () => {
   let component: MovieSearchComponent;
   let fixture: ComponentFixture<MovieSearchComponent>;
   const searchTerm = 'Die Hard';
-  const testData = <Movie[]>[
-    { 'title': 'Die Hard', 'year': '1988', 'imdbID': 'tt0095016', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzNmY2IwYzAtNDQ1NC00MmI4LThkOTgtZmVhYmExOTVhMWRkXkEyXkFqcGdeQXVyMTk5NDA3Nw@@._V1_SX300.jpg' },
-    { 'title': 'Live Free or Die Hard', 'year': '2007', 'imdbID': 'tt0337978', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BNDQxMDE1OTg4NV5BMl5BanBnXkFtZTcwMTMzOTQzMw@@._V1_SX300.jpg' },
-    { 'title': 'Die Hard with a Vengeance', 'year': '1995', 'imdbID': 'tt0112864', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BZjI0ZWFiMmQtMjRlZi00ZmFhLWI4NmYtMjQ5YmY0MzIyMzRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg' },
-    { 'title': 'Die Hard 2', 'year': '1990', 'imdbID': 'tt0099423', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzMzYzk3ZTEtZDg0My00MTY5LWE3ZmQtYzNhYjhjN2RhZGRjL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg' },
-    { 'title': 'Die Another Day', 'year': '2002', 'imdbID': 'tt0246460', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BODNkYmIwYTMtYzdhNy00YWE3LThkYmEtNzA5ZTE5YmVjYzZlXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg' },
-    { 'title': 'A Good Day to Die Hard', 'year': '2013', 'imdbID': 'tt1606378', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTcwNzgyNzUzOV5BMl5BanBnXkFtZTcwMzAwOTA5OA@@._V1_SX300.jpg' },
-    { 'title': 'A Million Ways to Die in the West', 'year': '2014', 'imdbID': 'tt2557490', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ0NDcyNjg0MV5BMl5BanBnXkFtZTgwMzk4NTA4MTE@._V1_SX300.jpg' },
-    { 'title': 'Live and Let Die', 'year': '1973', 'imdbID': 'tt0070328', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjI2NTY5MTYzMl5BMl5BanBnXkFtZTcwNzY5MDg0NA@@._V1_SX300.jpg' },
-    { 'title': 'John Tucker Must Die', 'year': '2006', 'imdbID': 'tt0455967', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3MDkwODQ3OV5BMl5BanBnXkFtZTcwNTE4NDQzMQ@@._V1_SX300.jpg' },
-    { 'title': 'Romeo Must Die', 'year': '2000', 'imdbID': 'tt0165929', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI5Nzg1MjA5M15BMl5BanBnXkFtZTYwNzAxNzg2._V1_SX300.jpg' }
-  ];
+  const testData = <PageableEntity<Movie>>{
+    entities: [
+      { 'title': 'Die Hard', 'year': '1988', 'imdbID': 'tt0095016', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzNmY2IwYzAtNDQ1NC00MmI4LThkOTgtZmVhYmExOTVhMWRkXkEyXkFqcGdeQXVyMTk5NDA3Nw@@._V1_SX300.jpg' },
+      { 'title': 'Live Free or Die Hard', 'year': '2007', 'imdbID': 'tt0337978', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BNDQxMDE1OTg4NV5BMl5BanBnXkFtZTcwMTMzOTQzMw@@._V1_SX300.jpg' },
+      { 'title': 'Die Hard with a Vengeance', 'year': '1995', 'imdbID': 'tt0112864', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BZjI0ZWFiMmQtMjRlZi00ZmFhLWI4NmYtMjQ5YmY0MzIyMzRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg' },
+      { 'title': 'Die Hard 2', 'year': '1990', 'imdbID': 'tt0099423', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzMzYzk3ZTEtZDg0My00MTY5LWE3ZmQtYzNhYjhjN2RhZGRjL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg' },
+      { 'title': 'Die Another Day', 'year': '2002', 'imdbID': 'tt0246460', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BODNkYmIwYTMtYzdhNy00YWE3LThkYmEtNzA5ZTE5YmVjYzZlXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg' },
+      { 'title': 'A Good Day to Die Hard', 'year': '2013', 'imdbID': 'tt1606378', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTcwNzgyNzUzOV5BMl5BanBnXkFtZTcwMzAwOTA5OA@@._V1_SX300.jpg' },
+      { 'title': 'A Million Ways to Die in the West', 'year': '2014', 'imdbID': 'tt2557490', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ0NDcyNjg0MV5BMl5BanBnXkFtZTgwMzk4NTA4MTE@._V1_SX300.jpg' },
+      { 'title': 'Live and Let Die', 'year': '1973', 'imdbID': 'tt0070328', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjI2NTY5MTYzMl5BMl5BanBnXkFtZTcwNzY5MDg0NA@@._V1_SX300.jpg' },
+      { 'title': 'John Tucker Must Die', 'year': '2006', 'imdbID': 'tt0455967', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3MDkwODQ3OV5BMl5BanBnXkFtZTcwNTE4NDQzMQ@@._V1_SX300.jpg' },
+      { 'title': 'Romeo Must Die', 'year': '2000', 'imdbID': 'tt0165929', 'type': 'movie', 'poster': 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTI5Nzg1MjA5M15BMl5BanBnXkFtZTYwNzAxNzg2._V1_SX300.jpg' }
+    ],
+    page: {
+      first: 1,
+      rows: 10
+    },
+    totalRecords: 73
+  };
   const observableResult = Observable.of(testData);
   const serviceMock = TypeMoq.Mock.ofType<MovieService>(MovieService, TypeMoq.MockBehavior.Loose);
   serviceMock.setup(x => x.search(TypeMoq.It.isValue(searchTerm)))
@@ -34,15 +45,16 @@ describe('MovieSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpModule],
-      providers: [
-        { provide: APP_BASE_HREF, useValue: '/' }, {
-          provide: MovieService, useValue: {
-            search: function (movieTitle: string) {
-              return serviceMock.object.search(movieTitle);
-            }
-          }
-        }],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        RouterModule,
+        InputTextModule,
+        DataListModule],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, {
+        provide: MovieService, useValue: {
+          search: (movieTitle: string) => serviceMock.object.search(movieTitle)
+        }
+      }],
       declarations: [MovieSearchComponent, MovieListComponent, MovieListItemComponent]
     }).compileComponents();
   }));
