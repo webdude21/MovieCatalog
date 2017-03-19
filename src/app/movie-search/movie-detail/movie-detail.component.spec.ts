74cd61549a6ae79c42eb1bf939d52d43ea0e7215
webdude <dimo.petrof@gmail.com>
2017-03-19 11:45:53 +0200

    still there are broken test, but it's a bit better

--- src/app/movie-search/movie-detail/movie-detail.component.spec.ts
+++ src/app/movie-search/movie-detail/movie-detail.component.spec.ts
@@ -1,4 +1,7 @@
-import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { MovieDetailResolver } from './movie-detail.resolver';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MovieDetail } from '../model/movie';
 import { RouterTestingModule } from '@angular/router/testing';
 import { APP_BASE_HREF } from '@angular/common';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
@@ -8,19 +11,49 @@ import { MovieDetailComponent } from './movie-detail.component';
 describe('MovieDetailComponent', () => {
   let component: MovieDetailComponent;
   let fixture: ComponentFixture<MovieDetailComponent>;
+  let movieDetail: MovieDetail = {
+    imdbRating: '8.2',
+    imdbVotes: '619,580',
+    imdbID: 'tt0095016',
+    title: 'Die Hard',
+    year: '1988',
+    rated: 'R',
+    released: '20 Jul 1988',
+    runtime: '131 min',
+    genre: 'Action, Thriller',
+    director: 'John McTiernan',
+    writer: 'Roderick Thorp (novel), Jeb Stuart (screenplay), Steven E. de Souza (screenplay)',
+    actors: 'Bruce Willis, Bonnie Bedelia, Reginald VelJohnson, Paul Gleason',
+    plot: 'John McClane, officer of the NYPD, tries to save his wife Holly Gennaro and several others that were taken hostage by German terrorist Hans Gruber during a Christmas party at the Nakatomi Plaza in Los Angeles.',
+    language: 'English, German, Italian',
+    country: 'USA', 'awards': 'Nominated for 4 Oscars. Another 6 wins & 2 nominations.',
+    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzNmY2IwYzAtNDQ1NC00MmI4LThkOTgtZmVhYmExOTVhMWRkXkEyXkFqcGdeQXVyMTk5NDA3Nw@@._V1_SX300.jpg',
+    metascore: '70',
+    type: 'movie',
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterModule, RouterTestingModule.withRoutes([
+        {
+          path: 'movie/:id',
+          component: MovieDetailComponent,
+          resolve: { movieDetail: MovieDetailResolver }
+        }])],
       declarations: [MovieDetailComponent],
-      providers: [ActivatedRoute, RouterTestingModule, { provide: APP_BASE_HREF, useValue: '/' }]
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, {
+        provide: ActivatedRoute,
+        useValue: {
+          params: Observable.of({ id: movieDetail.imdbID })
+        }
+      }]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MovieDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.movie = movieDetail;
   });
 
   it('should create', () => {
--- src/app/movie-search/service/movie.service.spec.ts
+++ src/app/movie-search/service/movie.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpModule } from '@angular/http';
 import { Movie } from '../model/movie';
 import { async, inject, TestBed } from '@angular/core/testing';
 import { MovieService } from './movie.service';
+import { PageableEntity } from '../model/pagable-entity';
 
 describe('MovieServiceService.search()', () => {
   beforeEach(() => {
@@ -16,7 +17,8 @@ describe('MovieServiceService.search()', () => {
   }));
 
   it('should return an Observable<Array<Movie>>', async(inject([MovieService], (service: MovieService) => {
-    service.search('Die Hard').subscribe((movies: Movie[]) => {
+    service.search('Die Hard').subscribe((pageableMovies: PageableEntity<Movie>) => {
+      const movies = pageableMovies.entities;
       expect(movies.length).toBe(10);
       expect(movies[0].title).toBe('Die Hard');
       expect(movies[5].title).toBe('Don\'t Die Too Hard!');
@@ -26,8 +28,8 @@ describe('MovieServiceService.search()', () => {
 
   it('should return an empty Observable<Array<Movie>> when the movie can\'t be found',
     async(inject([MovieService], (service: MovieService) => {
-      service.search('').subscribe((movies: Movie[]) => {
-        expect(movies.length).toBe(0);
+      service.search('').subscribe((pageableMovies: PageableEntity<Movie>) => {
+        expect(pageableMovies.entities.length).toBe(0);
       });
     })));
 });
