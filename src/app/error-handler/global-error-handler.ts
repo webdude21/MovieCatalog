002a15c311fe5946df79cc3f59578e56e0f1f23b
webdude <dimo.petrof@gmail.com>
2017-04-02 13:36:48 +0300

    fix indentetion in some of the files

--- src/app/error-handler/global-error-handler.ts
+++ src/app/error-handler/global-error-handler.ts
@@ -5,11 +5,11 @@ import { Message } from '../model/message';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-    constructor(private messageService: MessageService, private loggerService: LoggerService) { }
+  constructor(private messageService: MessageService, private loggerService: LoggerService) { }
 
-    handleError(error: any): void {
-        const message = new Message('Error', 'Service unavailable', 'error');
-        this.messageService.addMessage(message);
-        this.loggerService.log(message);
-    }
+  handleError(error: any): void {
+    const message = new Message('Error', 'Service unavailable', 'error');
+    this.messageService.addMessage(message);
+    this.loggerService.log(message);
+  }
 }
--- src/app/model/message.ts
+++ src/app/model/message.ts
@@ -1,5 +1,5 @@
 export type Severity = 'success' | 'info' | 'warn' | 'error' | 'multiple';
 
 export class Message {
-    constructor(public summary: string, public detail: string, public severity: Severity = 'info') { }
+  constructor(public summary: string, public detail: string, public severity: Severity = 'info') { }
 }
